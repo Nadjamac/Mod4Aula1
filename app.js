fa2a794497814e82eca302a5a9f2e97914e5c897
Nadja Machado <81447268+Nadjamac@users.noreply.github.com>
2021-08-19 21:46:08 -0300

    exemplo-calculadora

---
+++ app.js
@@ -0,0 +1,6 @@
+const calculadora = require ('./calculadora');
+
+console.log('soma:'+ calculadora.soma(1,2));
+console.log('subtr:' + calculadora.subt(5,3));
+console.log('mult:' + calculadora.mult(3,5));
+console.log ('div:')+  calculadora.div(10,2);
---
+++ calculadora.js
@@ -0,0 +1,29 @@
+const nome='calculadora';
+
+function soma( a , b ){
+    return a + b ;
+};
+
+function subt( a , b ){
+    return a - b ;
+};
+
+function multip( a , b ){
+    return a * b ;
+};
+
+function div( a , b ){
+    return a / b ;
+};
+
+
+
+module.exports={
+    soma,
+    subt,
+    multip,
+    div,
+    nome
+}
+
+// modulo - contem metodos funcões regras validacoes 
