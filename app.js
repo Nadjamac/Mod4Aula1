88b5be812c69b2290229617afbe829ba840c7454
Nadja Machado <81447268+Nadjamac@users.noreply.github.com>
2021-08-21 23:31:20 -0300

    exemplo http

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-console.log(' hellow world');
-console.log(' Meu nome  é Nadja ');
-console.log('Eu estou aprendendo node.js');
 
-// var
-// let
-//cons
-
-let idade =26;
-nome= 'Nadja' ;
-idade = 40;
-cons (monstarSite)='https://blueedtech.com.br/';
-
-if(monstrarSite){
-    console.log(site);
-}
-
-console.log('hello node.js!');
+const http = require('http');
+// console.log(http);
 
 
+http.createServer(function(req,res){
+    res.end('<h1>OLA SEJA BEM VINDO </h1>');
+}).listen(3000);  //  voce que indica a porta que vc deseja usar 
+
+console.log('Meu servidor esta rodando');
